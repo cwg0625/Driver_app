1d7b762eac2f91920c3a747331bff061bc35702a
Godmao <58934193+cwg0625@users.noreply.github.com>
2019-12-25 14:54:19 +0800

    Driver_app

---
+++ files/获取验证码/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m],n,_(o,p,q,r,s,t,u,_(),v,_(w,x,y,z,A,_(B,C,D,E),F,null,G,z,H,z,I,J,K,null,L,M,N,O,P,Q,R,M),S,_(),T,_(),U,_(V,[_(W,X,Y,j,Z,ba,q,bb,bc,bb,bd,be,v,_(bf,_(bg,bh,bi,bj),w,bk,R,M),S,_(),bl,_(),bm,g)])),bn,_(),bo,_(bp,_(bq,br)));}; 
+var b="url",c="获取验证码.html",d="generationDate",e=new Date(1577256446902.58),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="page",o="packageId",p="2c0fd5da68394cb59e3815ba9cadf25f",q="type",r="Axure:Page",s="name",t="获取验证码",u="notes",v="style",w="baseStyle",x="627587b6038d43cca051c114ac41ad32",y="pageAlignment",z="near",A="fill",B="fillType",C="solid",D="color",E=0xFFFFFFFF,F="image",G="imageHorizontalAlignment",H="imageVerticalAlignment",I="imageRepeat",J="auto",K="favicon",L="sketchFactor",M="0",N="colorStyle",O="appliedColor",P="fontName",Q="Applied Font",R="borderWidth",S="adaptiveStyles",T="interactionMap",U="diagram",V="objects",W="id",X="cb6c44ab4cc744eb93fb0a8fd36ac045",Y="label",Z="friendlyType",ba="矩形",bb="vectorShape",bc="styleType",bd="visible",be=true,bf="size",bg="width",bh=200,bi="height",bj=100,bk="4b7bfc596114427989e10bb0b557d0ce",bl="imageOverrides",bm="generateCompound",bn="masters",bo="objectPaths",bp="cb6c44ab4cc744eb93fb0a8fd36ac045",bq="scriptId",br="u66";
+return _creator();
+})());
